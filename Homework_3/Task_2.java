8d90c9bc45f8f91eb9ee7964a983d7237c76220f
Pavel <kryukov.p14@gmail.com>
2022-12-20 11:39:15 +0500

    task_2 issue

---
+++ Homework_3/Task_2.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Task_2 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nums= new ArrayList<>();
+        Iterator<Integer> iterator = nums.iterator();
+
+        for(int i = 0; i < 10; i++){
+            nums.add((int)(Math.random()*100));
+        }
+        System.out.println(nums);
+
+        for(int i = nums.size()-1; i >= 0; i--){
+            if (i%2 != 0){
+                nums.remove(i);
+        }
+        // System.out.println(nums);
+    }
+    System.out.println(nums);
+}
+}
